6e2c1b4943a32e7ff81347f34b1c9e05f8e98ba0
CactusLime <62575411+CactusLime@users.noreply.github.com>
2021-07-17 14:50:44 -0700

    imageIndex adds

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,5 +1,8 @@
 import React, {useState} from "react"
 export default () =>{
-    const imageStyle= {width: "95vw", maxWidth: "1280px", justifySelf: "center"};
-    return <img src={"/Maison3d.png"} style={imageStyle} />
+    const [imageIndex,setImageIndex]=useState(0);
+    const images=["/Maison3d.png","/Maison23d.png","Maison33d.png"]
+    
+    const imageStyle= {width: "95vw", maxWidth: "1280px", maxHeight: "95vh", justifySelf: "center"};
+    return <img onClick={setImageIndex(imageIndex+1)} src={images[imageIndex%images.length]} style={imageStyle} />
 }
