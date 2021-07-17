5e02eeb3e5416220baad4882555c332a9d8e5c6e
CactusLime <62575411+CactusLime@users.noreply.github.com>
2021-07-17 14:06:42 -0700

    title react components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React, {useState} from "react"
+import {Title} from "./Title"
 
 export default function Home() {
 const [title,setTitle]=useState("hello world")
-  return <div>{title}
+  return <div><Title title={title} />
   <input value={title} onChange={(e)=>setTitle(e.target.value)} /> 
   </div>
 }
+
+
