0808955d030ea932a826afb57cfe3520ecab1885
CactusLime <62575411+CactusLime@users.noreply.github.com>
2021-07-17 14:53:54 -0700

    semi-colon madness

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react"
+import React, {useState} from "react";
 export default () =>{
     const [imageIndex,setImageIndex]=useState(0);
-    const images=["/Maison3d.png","/Maison23d.png","Maison33d.png"]
+    const images=["/Maison3d.png","/Maison23d.png","/Maison33d.png"];
     
-    const imageStyle= {width: "95vw", maxWidth: "1280px", maxHeight: "95vh", justifySelf: "center"};
-    return <img onClick={setImageIndex(imageIndex+1)} src={images[imageIndex%images.length]} style={imageStyle} />
+    const imageStyle={width: "95vw", maxWidth: "1280px", maxHeight: "95vh", justifySelf: "center"};
+    return <img onClick={setImageIndex(imageIndex+1)} src={images[imageIndex%images.length]} style={imageStyle} />;
 }
